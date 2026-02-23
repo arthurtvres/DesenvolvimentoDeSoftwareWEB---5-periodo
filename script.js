b69c980ef7380e28a0d9f4170a2ce40d5f5b1d6e
arthurtavaresz <arthurzimtavares1512@outlook.com>
2026-02-23 20:03:28 -0300

    subindo atividade 2 de html, css e js.

---
+++ script.js
@@ -0,0 +1,27 @@
+const botaoTema = document.getElementById("temaBtn");
+
+botaoTema.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        botaoTema.innerText = "☀️ Light Mode";
+    } else {
+        botaoTema.innerText = "🌙 Dark Mode";
+    }
+});
+
+const form = document.getElementById("formContato");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const pokemon = document.getElementById("pokemonFav").value;
+
+    console.log("Nome:", nome);
+    console.log("Pokémon favorito:", pokemon);
+
+    document.getElementById("mensagem").innerText =
+        `Daora ${nome}, ${pokemon} é um ótimo pokémon!`;
+});
+
